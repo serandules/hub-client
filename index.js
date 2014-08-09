ae3ff6e6ffccfbfb49e28d13ccaef0f19fa2ba74
ruchiraw <ruchira.wageesha@gmail.com>
2014-08-09 22:56:53 +0530

    updating

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ var https = require('https');
 var fs = require('fs');
 var socproc = require('socproc-client');
 
-var plugins = [
-    require('./plugins/hub')
-];
-
 var HUB = 'hub.serandives.com:4000';
 
 var agent = new https.Agent({
@@ -17,7 +13,5 @@ var spc = socproc('server', agent, {
 });
 
 spc.on('connect', function (exec) {
-    plugins.forEach(function (plugin) {
-        plugin(exec);
-    });
+    require('./lib/hub')(exec);
 });
