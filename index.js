3398e387ee73272ce2135aac4680b4cac60811b7
ruchiraw <ruchira.wageesha@gmail.com>
2015-02-10 23:15:53 +0530

    updating

--- index.js
+++ index.js
@@ -1,47 +1,32 @@
-var debug = require('debug')('serandules:hub-client');
-var https = require('https');
-var fs = require('fs');
-var hub = require('./lib/hub');
-var socproc = require('socproc-client');
+var log = require('logger')('hub-client');
+var agent = require('hub-agent');
 
-var TOKEN = process.env.CLIENT_TOKEN;
+agent('hub-client', function () {
+    var https = require('https');
+    var fs = require('fs');
+    var hub = require('./lib/hub');
+    var socproc = require('socproc-client');
 
-var HUB = 'hub.serandives.com:4000';
+    var TOKEN = process.env.CLIENT_TOKEN;
 
-var started = false;
+    var HUB = 'hub.serandives.com:4000';
 
-var agent = new https.Agent({
-    key: fs.readFileSync('/etc/ssl/serand/hub-client.key'),
-    cert: fs.readFileSync('/etc/ssl/serand/hub-client.crt'),
-    ca: fs.readFileSync('/etc/ssl/serand/hub.crt')
-});
-
-var spc = socproc('server', agent, {
-    server: HUB,
-    query: 'token=' + TOKEN
-});
-
-spc.on('connect', function (exec) {
-    hub(exec);
-});
-
-spc.on('reconnect', function (exec) {
-    hub.reconnect(exec);
-});
-
-process.on('uncaughtException', function (err) {
-    debug('unhandled exception ' + err);
-    debug(err.stack);
-    if (started) {
-        return;
-    }
-    process.send({
-        event: 'hub-client stopped'
+    var agent = new https.Agent({
+        key: fs.readFileSync('/etc/ssl/serand/hub-client.key'),
+        cert: fs.readFileSync('/etc/ssl/serand/hub-client.crt'),
+        ca: fs.readFileSync('/etc/ssl/serand/hub.crt')
     });
-});
 
-started = true;
+    var spc = socproc('server', agent, {
+        server: HUB,
+        query: 'token=' + TOKEN
+    });
 
-process.send({
-    event: 'hub-client started'
-});
+    spc.on('connect', function (exec) {
+        hub(exec);
+    });
+
+    spc.on('reconnect', function (exec) {
+        hub.reconnect(exec);
+    });
+}, 1);
