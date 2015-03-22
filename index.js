7f0a18c53f5b5dfe96e18d976b9ef34b81a9e02d
ruchiraw <ruchira.wageesha@gmail.com>
2015-03-23 00:21:36 +0530

    updating

--- index.js
+++ index.js
@@ -7,49 +7,47 @@ var agent = require('hub-agent');
 var drones = {};
 
 agent('/servers', function (err, io) {
-    io.once('connect', function () {
-        io.emit('unmarshal');
+    io.emit('unmarshal');
 
-        io.on('up', function () {
-            log.debug('server up request');
-            procevent.emit('up');
+    io.on('up', function () {
+        log.debug('server up request');
+        procevent.emit('up');
+    });
+
+    io.on('start', function (id, domain, repo) {
+        droner.start(id, repo, 'index.js', function (err, process, port) {
+            if (err) {
+                return log.error('drone startup error | id:%s, error:%s', id, err);
+            }
+            drones[id] = {
+                id: id,
+                domain: domain,
+                pid: process.pid,
+                port: port
+            };
+            io.emit('started', id, domain, process.pid, port);
+            log.debug('drone started | id:%s, pid:%s, port:%s', id, process.pid, port);
         });
+    });
 
-        io.on('start', function (id, domain, repo) {
-            droner.start(id, repo, 'index.js', function (err, process, port) {
-                if (err) {
-                    return log.error('drone startup error | id:%s, error:%s', id, err);
-                }
-                drones[id] = {
-                    id: id,
-                    domain: domain,
-                    pid: process.pid,
-                    port: port
-                };
-                io.emit('started', id, domain, process.pid, port);
-                log.debug('drone started | id:%s, pid:%s, port:%s', id, process.pid, port);
-            });
+    io.on('stop', function (id) {
+        droner.stop(id, function (err) {
+            if (err) {
+                return log.error('drone stop error | id:%s, error:%s', id, err);
+            }
+            delete drones[id];
+            io.emit('stopped', id);
+            log.debug('drone stopped | id:%s', id);
         });
+    });
 
-        io.on('stop', function (id) {
-            droner.stop(id, function (err) {
-                if (err) {
-                    return log.error('drone stop error | id:%s, error:%s', id, err);
-                }
-                delete drones[id];
-                io.emit('stopped', id);
-                log.debug('drone stopped | id:%s', id);
-            });
-        });
-
-        io.on('restart', function (id) {
-            var drone = drones[id];
-            droner.restart(id, function (err, process, port) {
-                drone.pid = process.pid;
-                drone.port = port;
-                io.emit('restarted', id, drone.domain, drone.pid, drone.port);
-                log.debug('drone restarted | id:%s', id);
-            });
+    io.on('restart', function (id) {
+        var drone = drones[id];
+        droner.restart(id, function (err, process, port) {
+            drone.pid = process.pid;
+            drone.port = port;
+            io.emit('restarted', id, drone.domain, drone.pid, drone.port);
+            log.debug('drone restarted | id:%s', id);
         });
     });
 
@@ -62,8 +60,7 @@ procevent.emit('started', 0);
 
 log.info('hub-client started | pid:%s', process.pid);
 
-/*
- process.on('uncaughtException', function (err) {
- log.fatal('unhandled exception %s', err);
- log.trace(err.stack);
- });*/
+process.on('uncaughtException', function (err) {
+    log.fatal('unhandled exception %s', err);
+    log.trace(err.stack);
+});
